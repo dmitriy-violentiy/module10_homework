e79f5e43639e8faef54d0e255a2535437989bcab
dmitriy-violentiy <65154557+dmitriy-violentiy@users.noreply.github.com>
2022-12-22 16:00:48 +0300

    Add files via upload

---
+++ 2/index.js
@@ -0,0 +1,6 @@
+const but = document.getElementById('but')
+
+but.addEventListener('click', function() {
+   alert(`Ширина экрана: ${window.screen.width}
+Высота экрана: ${window.screen.height}`)
+})
